59cff68e5bb9159a7030518c0f61b929572de673
parsifal <crawshawh04@gmail.com>
2022-10-06 16:25:40 +0100

    Add Best Time to Buy and Sell Stock

--- Two Pointers/Best Time to Buy and Sell Stock.py
+++ Two Pointers/Best Time to Buy and Sell Stock.py
@@ -1,3 +1,15 @@
+prices = [2, 1, 4, 6, 7, 2, 8, 6, 7]
 
+def mostProfit(prices):
+  l, r = 0, 1
+  max_profit = 0
 
+  while r < len(prices):
+    if prices[r] > prices[l]:
+      current_profit = prices[r] - prices[l]
+      max_profit = max(max_profit, current_profit)
+    else:
+      l = r
+    r += 1
 
+  return max_profit
