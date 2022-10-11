e9442a3196cf5fcf2deab6e85c53265076160f07
parsifal <crawshawh04@gmail.com>
2022-10-11 02:01:34 +0100

    Add Binary Tree stuff

--- sketchpad.py
+++ sketchpad.py
@@ -1,15 +1,25 @@
-def twosumordered(target, nums):
-  l, r = 0, len(nums) - 1
+s = "abcabcbb"
 
-  while l < r:
-    twosum = nums[l] + nums[r]
-    if twosum > target:
-      r -= 1
-    elif twosum < target:
-      l += 1
-    else:
-      return [l + 1, r + 1]
+l, r = 0, 2
+subslen = 0
 
-numberss = [2,7,11,15]
-targett = 9
-print(twosumordered(targett, numberss))
+def maxsubslen(s):
+  l, r = 0, 1
+  substring_length = 0
+  
+  while r < len(s):
+    subs = s[l]
+    while r < len(s) and s[r] not in subs:
+      subs += s[r]
+      r += 1
+      current_subslength = r - l
+      substring_length = max(substring_length, current_subslength)
+    if subs[r] in subs:
+      l = r
+  return substring_length
+
+print(maxsubslen(s))
+
+  
+
+
